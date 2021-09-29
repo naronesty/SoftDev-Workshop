ae7c8deccd8636ce43f7f6d99e9e52353964236a
Noakai Aronesty <74675547+naronesty@users.noreply.github.com>
2021-09-28 21:43:11 -0400

    Undid HW 05 commit

--- 01_py/01names.py
+++ 01_py/01names.py
@@ -1,35 +1,10 @@
-# SUMMARY OF TRIO DISCUSSION
-# We discussed the lists of names and how we had to change each element. We also discussed which methods were more concise and picked that to use. We combined the most concise way to print a name with the method of using a function. We also discussed adding input so that the user can choose a class.
-# DISCOVERIES
-# We each discovered other ways to pick and print a name at random. There was randrange, random.choice, and even random.randint. We also discovered a way to check for empty inputs so that if the user doesn't pick a class, the program can print a name from either class at random.
-# QUESTIONS
-# There were no questions.
-# COMMENTS
-# It was insightful to collaborate with other people and gain new ideas for this python program.
-
 import random
-
-# List people for periods 1 and 2
-KREWES = {
-    'orpheus': ["Alejandro Alonso", "Aryaman Goenka", "Christopher Liu", "Deven Maheshwari", "Emma Buller", "Ella Krechmer", "Gavin McGinley",
-           "Haotian Gan", "Ivan Lam", "Ishraq Mahid", "Ivan Mijacika", "Julia Nelson", "Lucas Lee", "Lucas Tom wong",
-           "Michelle Lo", "Oscar Wang", "Owen Yaggy", "Rayat Roy", "Reng geng Zheng", "Shriya Anand", "Shyne Choi", "Sadid Ethun",
-           "Tomas Acuna", "Theodore Fahey", "Tina Nguyen", "Tami Takada", "William Chen", "Yusef Elsharawy", "Zhao yu Lin"],
-    'endymion': ["Alif Abdullah", "Andrew Juang", "Andy Lin", "Austin Ngan", "Annabel Zhang", "Cameron Nelson", "David Chong",
-            "Daniel Sooknanan", "Eric Guo", "Eliza Knapp", "Hebe Huang", "Han Zhang", "Yoonah Chang", "Joshua Kloepfer",
-            "Josephine Lee", "Jonathan Wu", "Jesse Xie", "Justin Zou", "Kevin Cao", "Liesel Wong", "Michael Borczuk",
-            "Noakai Aronesty", "Patrick Ging", "Qina Liu", "Roshani Shrestha", "Rachel Xiao", "Raymond Yeung", "Sophie Liu",
-            "Shadman Rakib", "Thomas Yu", "Wen hao Dong", "Yaying Liang li", "Yuqing Wu"]
-}
-
-def random_student():
-    val = input("Which class? class1 or class2\n")
-    if (val == "class1"):
-        print(random.choice(KREWES['orpheus']))
-    elif (val == "class2"):
-        print(random.choice(KREWES['endymion']))
-    else:
-        random_class = random.choice([KREWES['orpheus'], KREWES['endymion']])
-        print(random.choice(random_class))
-
-random_student()
+#Lists of period 1 and 2
+pd1 = ['Owen Yaggy', 'Haotain Gan', 'Ishraq Mahid', 'Kevin Cao', 'Ivan Lam', 'Michelle Lo', 'Christopher Liu', 'Michael Borzuk', 'Ivan Mijacika', 'Lucas Lee', 'Rayat Roy', 'Emma Buller', 'Andrew Juang', 'Renggeng Zheng', 'Annabel Zhang', 'Alejandro Alonso', 'Deven Mahehwari', 'David', 'Aryaman Goenka', 'Oscar Wang', 'Tami Takada', 'Yusuf Elsharawy', 'Ella Krechmer', 'Tomas Acuna', 'Tina Nguyen', 'Sadid Ethun', 'Shyne Choi', 'Shriya Anand', 'lol28', 'lol29', 'lol30', 'lol31']
+pd2 = ['Patrick Ging', 'Raymond Yeung', 'Josephine Lee', 'Alif Abdullah', 'William Chen', 'Justin Zou', 'Andy Lin', 'Rachel Xiao', 'Yuqing Wu', 'Shadman Rakib', 'Liesel Wong', 'Daniel Sooknanan', 'Cameron Nelson', 'Austin Ngan', 'Thomas Yu', 'Yaying Liang Li', 'Jesse Xie', 'Eric Guo', 'Jonathan Wu', 'Zhaoyu Lin', 'Joshua Kloepfer', 'Noakai Aronesty', 'Yoonah Chang', 'Hebe Huang', 'Wen Hao Dong', 'Mark Zhu', 'lul26', 'lul27', 'lul28', 'lul29', 'lul30', 'lul31']
+#random function to pick a random person in the class
+a = random.randint(0,32)
+b = random.randint(0,32)
+#Prints the list indices corresponding to the random number 
+print("pd1: " + pd1[a])
+print("pd2: " + pd2[b])
