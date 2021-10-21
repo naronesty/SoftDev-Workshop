480a3b3061fae360bab6a09d5dce7d492c31294a
Noakai Aronesty <74675547+naronesty@users.noreply.github.com>
2021-10-21 19:11:11 -0400

    Update db_builder.py

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -1,21 +1,10 @@
-#Clyde "Thluffy" Sinclair
+#Noakai Aronesty
 #SoftDev
-#skeleton/stub :: SQLITE3 BASICS
-#Dec 2020 -- The Time of the Rona
+#K16 -- All About Database
+#2021-10-21
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
-
-courses = {}
-
-with open('courses.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-
-        # for row in reader:
-        #     courses[row['code']] = list(row['mark'],row[''])
-
-
-
 
 DB_FILE="discobandit.db"
 
@@ -27,17 +16,39 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 # < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
 
+with open('courses.csv', newline='') as csvfile0:
+    readerC = csv.DictReader(csvfile0)
+    command = ""          # test SQL stmt in sqlite3 shell, save as string
+    c.execute(command)    # run SQL statement
+    c.execute("CREATE TABLE IF NOT EXISTS courses (code TEXT, mark INTEGER, id INTEGER);")
+    for row in readerC:
+        c.execute("INSERT INTO courses VALUES (\"" +row['code']+ "\"," +row['mark']+"," +row['id']+ ");")
+with open('students.csv', newline='') as csvfile1:
+    readerS = csv.DictReader(csvfile1)
+    c.execute(command)    # run SQL statement
+    c.execute("CREATE TABLE IF NOT EXISTS students (name TEXT, age INTEGER, id INTEGER);")
+    for row in readerS:
+        c.execute("INSERT INTO students VALUES (\"" +row['name']+ "\"," +row['age']+"," +row['id']+ ");")
 
-command = ""          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
-c.execute("CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER);")
-for row in reader:
-    c.execute("INSERT INTO courses VALUES (?, ?, ?)", row['code'], row['mark'], row['id'])
+#TESTING courses import:
+c.execute("SELECT * FROM courses")
+colnames = c.description
+for line in colnames:
+    print(line[0])
+rows = c.fetchall()
+for row in rows:
+    print(row)
 
+print("\n\n")
+#TESTING students import:
+c.execute("SELECT * FROM students")
+colnames = c.description
+for line in colnames:
+    print(line[0])
+rows = c.fetchall()
+for row in rows:
+    print(row)
 #==========================================================
 
 db.commit() #save changes
 db.close()  #close database
-
-if __name__ == "__main__":
-    fileReader()
