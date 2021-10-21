850dc352ae0dd4010e9d825103f6f53390108432
Noakai Aronesty <noakai@gmail.com>
2021-10-21 18:14:45 -0400

    k16 update

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -1,10 +1,20 @@
 #Clyde "Thluffy" Sinclair
-#SoftDev  
+#SoftDev
 #skeleton/stub :: SQLITE3 BASICS
 #Dec 2020 -- The Time of the Rona
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
+
+courses = {}
+
+with open('courses.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+
+        # for row in reader:
+        #     courses[row['code']] = list(row['mark'],row[''])
+
+
 
 
 DB_FILE="discobandit.db"
@@ -20,10 +30,14 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 command = ""          # test SQL stmt in sqlite3 shell, save as string
 c.execute(command)    # run SQL statement
+c.execute("CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER);")
+for row in reader:
+    c.execute("INSERT INTO courses VALUES (?, ?, ?)", row['code'], row['mark'], row['id'])
 
 #==========================================================
 
 db.commit() #save changes
 db.close()  #close database
 
-
+if __name__ == "__main__":
+    fileReader()
