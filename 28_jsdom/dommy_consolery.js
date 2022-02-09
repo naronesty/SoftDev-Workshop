e1dc3e1b7d89a48f6d03d63fad131f9e64048bda
Noakai Aronesty <74675547+naronesty@users.noreply.github.com>
2022-02-08 21:50:36 -0500

    Update dommy_consolery.js

--- 28_jsdom/dommy_consolery.js
+++ 28_jsdom/dommy_consolery.js
@@ -11,6 +11,10 @@ function fact(n) {
     else return n * fact(n - 1)
 }
 
+fact(4)
+fact(5)
+fact(10)
+
 function helpy(first, second, remain) {
     if (remain == 0) return second
     else if (remain == 1) return first
@@ -21,8 +25,16 @@ function fib(n) {
     return helpy(1, 1, (n))
 }
 
+fib(3)
+fib(5)
+fib(99)
+
 function gcd(a, b) {
     if (a == b) return a
     else if (a > b) return gcd((a - b), b)
     else return gcd(a, (b - a))
 }
+
+gcd(48, 18)
+gcd(18, 48)
+gcd(25, 125)
